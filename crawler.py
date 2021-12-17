1affb8dd1c0d2ceb528ac9d9563ae7b0fe7224b4
Othman Bouzahzah <othmanbouzahzah@gmail.com>
2021-12-17 14:38:00 +0100

    initial commit

---
+++ crawler.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+from utils import *
+import json
+import pandas as pd
+
+urls_to_scrape = [base_url+domain.lower() for domain in domains]
+
+quotes_metadata = []
+
+for url in urls_to_scrape:
+    
+    content = requests.get(url, verify=True).content
+    soup = BeautifulSoup(content, "html.parser")
+    quotes_divs = soup.find_all('div', {'class': 'border-bottom my-2'})
+    print('fetching this domain : ',url[45:])
+    for quote_div in quotes_divs:
+        quotes_metadata_tmp = {}
+
+        domain = url[45:]
+        quotes_metadata_tmp['domain'] = domain
+
+        article_title = quote_div.find('h4').get_text()        
+        quotes_metadata_tmp['article_title'] = article_title
+
+        date = quote_div.find('h5').get_text()
+        quotes_metadata_tmp['date'] = date
+
+        quote = quote_div.find('p').get_text()
+        quotes_metadata_tmp['quote'] = quote
+    
+        quotes_metadata.append(quotes_metadata_tmp)
+
+print(type(quotes_metadata))
+with open('quotes.txt', mode='wt', encoding='utf-8') as myfile:
+    myfile.write(str(quotes_metadata))
+
+df = pd.DataFrame(quotes_metadata)
+
+df.to_csv('satoshi_quotes.csv')
---
+++ utils.py
@@ -0,0 +1,11 @@
+base_url = 'https://satoshi.nakamotoinstitute.org/quotes/'
+
+domains = ['Addresses', 'Banks',
+           'Bitcoin Design', 'Bitcoin Economics', 'Cryptocurrency',
+           'Difficulty', 'Double Spending', 'Economics', 'Encryption',
+           'Fees', 'General', 'Government',
+           'Identity', 'Micropayments', 'Mining', 'Motives', 'Nodes',
+           'P2P Networks', 'Privacy', 'Proof-of-Work', 'Releases',
+           'Scalability', 'Transactions', 'Trusted Third Parties']
+
+           
